c751e77044c39c7ec9f4bb536d0685c36a20e683
William Mallett <mallett002@gmail.com>
2019-12-02 19:03:33 -0600

    adding Exercise model

--- src/main/java/com/pluralsight/controller/MinutesController.java
+++ src/main/java/com/pluralsight/controller/MinutesController.java
@@ -1,13 +1,17 @@
 package com.pluralsight.controller;
 
+import com.pluralsight.model.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MinutesController {
 
     @RequestMapping(value = "/addMinutes")
-    public String addMinutes() {
+    public String addMinutes(@ModelAttribute ("exercise") Exercise exercise) {
+        System.out.println("exercise: " + exercise.getMinutes());
+
         return "add minutes";
     }
 }
